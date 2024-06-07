39974336c2f2fe3ef058de95c2c7622a9e74fa12
C0deWeather <frankbright211@gmail.com>
2024-06-07 01:07:07 +0100

    siblings and uncles

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,14 +1,14 @@
 #include "binary_trees.h"
 
-/** 
- * binary_tree_sibling = finds the sibling of a nodee
- * @tree: a pointer to the node to find the sibling
+/**
+ * binary_tree_sibling - finds the sibling of a node
+ * @node: a pointer to the node to find the sibling
  *
  * Return: a pointer to the sibling, otherwise NULL.
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	binary_tree_t *parent ;
+	binary_tree_t *parent;
 
 	if (!node || !node->parent)
 	{
--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -2,25 +2,26 @@
 
 /**
  * binary_tree_uncle - finds the uncle of a node
- * @tree: a pointer to the node to find the sibling
+ * @node: a pointer to the node to find the sibling
  *
  * Return: a pointer to the sibling, otherwise NULL.
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-    binary_tree_t *parent;
+	binary_tree_t *parent;
 	binary_tree_t *grandparent;
 
-    if (!node || !node->parent)
+	if (!node || !node->parent)
 	{
-        return (NULL);
+		return (NULL);
 	}
 	parent = node->parent;
 	grandparent = parent->parent;
-	
-    if (!grandparent || !grandparent->left ||
-		!grandparent->right)
-        return (NULL);
 
-    return (parent == grandparent->left ? grandparent->right : grandparent->left);
+	if (!grandparent || !grandparent->left ||
+		!grandparent->right)
+		return (NULL);
+
+	return (parent == grandparent->left ?
+			grandparent->right : grandparent->left);
 }
